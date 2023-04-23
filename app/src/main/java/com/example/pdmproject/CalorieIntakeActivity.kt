12c7ab8d8344f28bab9709e34b010a73cecb6ff4
dariusalba <paul.darius34@yahoo.com>
2023-04-23 23:06:50 +0300

    removed useless

--- app/src/main/java/com/example/pdmproject/CalorieIntakeActivity.kt
+++ app/src/main/java/com/example/pdmproject/CalorieIntakeActivity.kt
@@ -11,16 +11,12 @@ class CalorieIntakeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calorie_intake)
 
-        // Retrieve the daily calorie intake value passed from the previous activity
         val dailyCalorieIntake = intent.getDoubleExtra("DAILY_CALORIE_INTAKE", 0.0)
 
-        // Find the calorieIntakeTextView TextView
         val calorieIntakeTextView = findViewById<TextView>(R.id.calorieIntakeTextView)
 
-        // Display the daily calorie intake value in the TextView
         calorieIntakeTextView.text = "Your daily calorie intake should be: $dailyCalorieIntake"
 
-        // Find the backButton button and set a click listener to finish the activity and return to the previous activity
         val backButton = findViewById<Button>(R.id.backButton)
         backButton.setOnClickListener {
             finish()
--- app/src/main/java/com/example/pdmproject/WeightManagementActivity.kt
+++ app/src/main/java/com/example/pdmproject/WeightManagementActivity.kt
@@ -14,17 +14,15 @@ class WeightManagementActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weight_management)
 
-        // Find the calculate button
         val calculateButton = findViewById<Button>(R.id.submitButton)
 
-        // Set a click listener on the button
         calculateButton.setOnClickListener {
-            // Get the user's weight management goal
+
             val goalRadioGroup = findViewById<RadioGroup>(R.id.goalRadioGroup)
             val selectedGoalId = goalRadioGroup.checkedRadioButtonId
             val selectedGoal = findViewById<RadioButton>(selectedGoalId).text.toString()
 
-            // Get the user's age, height, and weight
+
             val ageEditText = findViewById<EditText>(R.id.ageEditText)
             val age = ageEditText.text.toString().toInt()
 
@@ -34,20 +32,16 @@ class WeightManagementActivity : AppCompatActivity() {
             val weightEditText = findViewById<EditText>(R.id.weightEditText)
             val weight = weightEditText.text.toString().toDouble()
 
-            // Calculate the user's daily calorie intake based on their weight management goal
             val dailyCalorieIntake = if (selectedGoal == "Weight Loss") {
                 (10 * weight) + (6.25 * height) - (5 * age) - 500
             } else {
                 (10 * weight) + (6.25 * height) - (5 * age) + 500
             }
 
-            // Create an Intent to start the CalorieIntakeActivity
             val intent = Intent(this, CalorieIntakeActivity::class.java)
 
-            // Pass the daily calorie intake value to the CalorieIntakeActivity
             intent.putExtra("DAILY_CALORIE_INTAKE", dailyCalorieIntake)
 
-            // Start the CalorieIntakeActivity
             startActivity(intent)
         }
     }
